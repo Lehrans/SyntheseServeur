b090df998854a984a937a66cf9a1a2dfa842eff2
simon <sim_bro1001@hotmail.ca>
2020-12-21 11:49:21 -0500

    Essaie d'upload pour evenNode

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ import chalk from 'chalk';
 
 import app from './src/app.js';
 
-const PORT = 5600;
-
-
-
-app.listen(PORT, err => {
+app.listen(process.env.PORT, err => {
     // Mon serveur est maintenant en mode écoute
-    console.log(chalk.blue(`Mon serveur est démarré et écoute sur le port ${PORT}`));
+    console.log(chalk.blue(`Mon serveur est démarré et écoute sur le port ${process.env.PORT}`));
 });
